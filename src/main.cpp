36ebb5fc2ab01d739d5fdb3407e8616e97a25261
Sandiep <sandeepsondagar@gmail.com>
2022-09-21 11:42:36 +0530

    test coverage

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,28 @@
+#if defined(ARDUINO)
 #include <Arduino.h>
 
 // cppcheck-suppress unusedFunction
 void setup() {
-  // put your setup code here, to run once:
-  uint8_t *ptr1 = (uint8_t*)malloc(512);
-  uint8_t *ptr2 = (uint8_t*)malloc(512);
-  uint8_t *ptr3 = (uint8_t*)malloc(512);
+    // put your setup code here, to run once:
+    uint8_t *ptr1 = (uint8_t *)malloc(512);
+    uint8_t *ptr2 = (uint8_t *)malloc(512);
+    uint8_t *ptr3 = (uint8_t *)malloc(512);
 
-  (void)ptr1;
-  (void)ptr2;
-  (void)ptr3;
+    (void)ptr1;
+    (void)ptr2;
+    (void)ptr3;
 
-  free(ptr1);
-  free(ptr2);
-  free(ptr3);
+    free(ptr1);
+    free(ptr2);
+    free(ptr3);
 }
 
 // cppcheck-suppress unusedFunction
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
 }
+#else
+int main(int argc, char **argv) {
+    return 0;
+}
+#endif
