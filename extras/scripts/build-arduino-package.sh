3eb1c79dad5180545445c6da3418ad49c6e3a03f
Sandiep <sandeepsondagar@gmail.com>
2022-09-26 10:26:58 +0530

    release added

---
+++ extras/scripts/build-arduino-package.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eu
+
+INPUT=$1
+OUTPUT=$2
+
+cd "$INPUT"
+
+# remove existing file
+rm -f "$OUTPUT"
+
+# create zip
+7z a "$OUTPUT" \
+	-xr!.vs \
+	src \
+    lib \
+    include \
+    test \
+	README.md \
